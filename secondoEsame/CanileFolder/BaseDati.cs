5d25188192427c8c643908624d0f2a1bc78312c0
ardit <arditllaha@gmail.com>
2021-04-21 23:24:09 +0200

    Initial commit

---
+++ secondoEsame/CanileFolder/BaseDati.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace secondoEsame.CanileFolder
+{
+    public class BaseDati : DbContext
+    {
+        public DbSet<Canile> canili { get; set; }
+
+        public BaseDati(DbContextOptions options) : base(options)
+        { }
+    }
+}
---
+++ secondoEsame/CanileFolder/Canile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace secondoEsame.CanileFolder
+{
+    public class Canile
+    {
+        [Key]
+        public int Id_cane  {get; set;}
+
+        [Required(ErrorMessage = "Devi scegliere un nome!")]
+        public string Nome_cane { get; set; }
+
+        [Required(ErrorMessage = "Devi selezionare la data di nascita!")]
+        public DateTime Anno_nascita { get; set; }
+
+        [Required(ErrorMessage = "Devi scegliere la razza!")]
+        public string Razza { get; set; }
+
+        [Required(ErrorMessage = "Devi scegliere il periodo!")]
+        public bool Genere { get; set; }
+
+        [Required(ErrorMessage = "Devi scegliere il periodo!")]
+        public DateTime Data_addozione { get; set; }
+
+        [Required(ErrorMessage = "Devi scegliere il periodo!")]
+        public DateTime Data_acquisizione { get; set; }
+
+        [Required(ErrorMessage = "Devi scegliere il periodo!")]
+        public bool Sterilizato { get; set; }
+
+        [Required(ErrorMessage = "Devi scegliere il periodo!")]
+        public DateTime Data_steril { get; set; }
+    }
+}
